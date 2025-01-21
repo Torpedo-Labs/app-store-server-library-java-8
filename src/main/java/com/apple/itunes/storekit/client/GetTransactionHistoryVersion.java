115084226d8e0b68fb07f66a267c4264ffb5c50a
Sihan Foyez <sihan@torpedolabs.com>
2025-01-21 17:58:18 +0600

    Java8 support - removed since in Deprecated annotation

--- src/main/java/com/apple/itunes/storekit/client/GetTransactionHistoryVersion.java
+++ src/main/java/com/apple/itunes/storekit/client/GetTransactionHistoryVersion.java
@@ -2,7 +2,8 @@ package com.apple.itunes.storekit.client;
 
 public enum GetTransactionHistoryVersion {
 
-    @Deprecated(since = "2.2.0")
+//    @Deprecated(since = "2.2.0")
+    @Deprecated
     V1("v1"),
     V2("v2");
 
